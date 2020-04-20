e893494873b1060bf0606b8355fe1f1a8843efb4
Ian Hooper <ianhooper14@gmail.com>
2020-04-19 20:23:19 -0400

    Updates

---
+++ precache-manifest.e79b13c24e142e297dd7d7b79586d582.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f196cbab81ea48bd4e26ff68d798645",
+    "url": "/mlb_clickygame/index.html"
+  },
+  {
+    "revision": "49b90a85d35de4397fe8",
+    "url": "/mlb_clickygame/static/css/main.283f4ab9.chunk.css"
+  },
+  {
+    "revision": "1e4b4fa84ecc0f8921f3",
+    "url": "/mlb_clickygame/static/js/2.5375ae4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mlb_clickygame/static/js/2.5375ae4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b90a85d35de4397fe8",
+    "url": "/mlb_clickygame/static/js/main.60675a31.chunk.js"
+  },
+  {
+    "revision": "a8cf7773b27948642c15",
+    "url": "/mlb_clickygame/static/js/runtime-main.18378236.js"
+  }
+]);
